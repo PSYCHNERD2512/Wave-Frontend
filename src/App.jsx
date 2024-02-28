de8804780aa34e21d0c1dc88e186b4e2545ddfcb
tanishqmm <tanishqmandhane@gmail.com>
2024-02-28 23:18:36 +0530

    Done heading and left(navigation and connections) part of home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+import logos from './assets/logo_wave.png'
+import home from './assets/home.png'
+import profile from './assets/profile.png'
+import setting from './assets/setting.png'
+import avtar from './assets/Avatar.png'
+function Wave() {
+  
+  return(<>
+    <Header/>
+    <div id="content">
+      <div id="sideBar"> 
+        <div id="sideupper">
+          <div id="home">        
+            <Navigation_icons logo={home} name="  Home"/>
+          </div>
+            <Navigation_icons logo={profile} name="  Profile"/>
+            <Navigation_icons logo={setting} name="  Settings"/>          
+        </div>
+        <hr />
+        <div className='connections'>
+          My Connections
+          <div id="myconnec">64</div>
+        </div>
+        <div id="sidelower">
+          <Avtars name="Tanishq" connectionsNum={69}/>
+          <Avtars name="Tanishq" connectionsNum={69}/>
+          <Avtars name="Tanishq" connectionsNum={69}/>
+          <Avtars name="Tanishq" connectionsNum={69}/>
+          <Avtars name="Tanishq" connectionsNum={69}/>
+        </div>
+        <div id="seeall">See All</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="right">
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
-  )
+    );
 }
 
-export default App
+function Header(){
+  return(
+    <div className = "Header">
+      
+      <div id="head">
+        <img id="logo" src={logos}/>
+        <div id="WAVE">Wave</div>
+      </div>
+    
+    </div>
+  );
+}
+
+function Navigation_icons({logo,name}) {
+  return(
+    <div className="nav">
+      <img src={logo} alt="" />{name}
+    </div>
+  );
+}
+function Avtars({name,connectionsNum}){
+  return(
+    <div className='profile'>
+      <img src={avtar} alt="" />
+      <div id="details">
+        <div className='name'>{name}</div>
+        <div >{connectionsNum}M connections</div>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default Wave
