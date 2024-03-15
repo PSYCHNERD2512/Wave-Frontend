939e4f862ffa9eaa883b385bc6b6cf5afdd0772d
tanishqmm <tanishqmandhane@gmail.com>
2024-03-15 20:02:51 +0530

    added router

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Wave from './App.jsx'
 import Login from './login.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Wave/>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Wave />
-    {/* <Login/> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
